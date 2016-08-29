db6ced8c745685eacd33e9b76dd62d25bf366e21
clen <chencl@jiagouyun.com>
2016-08-29 17:09:24 +0800

    反射

---
+++ src/main/java/com/ccl/test/Test.java
@@ -0,0 +1,18 @@
+package com.ccl.test;
+
+/**
+ * Created by ccl on 16/8/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        // List<String> list = new ArrayList<String>();
+        // list.add("A");
+        // list.add("B");
+        // list.add("C");
+        // list.add("D");
+
+        // // 根据不同切割符返回字符串，list.size()为0时返回""
+        // System.out.println(StringUtils.join(list.toArray(), ";"));
+        System.out.println("Hello World !");
+    }
+}
